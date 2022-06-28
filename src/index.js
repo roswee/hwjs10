b01662b55eefcb64c44d49db43641786126990e0
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-28 18:33:27 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
-
+import { fetchCountries } from "./js/fetchCountries"
 const DEBOUNCE_DELAY = 300;
+
+var debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
+
+const inputCountry = document.querySelector("#search-box");
+
+inputCountry.addEventListener("input", debounce(eventHandle, DEBOUNCE_DELAY));
+
+function eventHandle() {
+    if (!inputCountry === null) {
+        fetchCountries(inputCountry.value)
+    }
+    return
+}
+
