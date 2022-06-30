8ffe61128f04dd84245e88d2c979663f2c939580
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-30 11:01:19 +0200

    almost complete

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
-const DEBOUNCE_DELAY = 300;
-
-var debounce = require('lodash.debounce');
+import {debounce} from 'lodash';
 import Notiflix from 'notiflix';
+const DEBOUNCE_DELAY = 300;
 
 const inputCountry = document.querySelector("#search-box");
 const countryList = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info")
 inputCountry.addEventListener("input", debounce(eventHandle, DEBOUNCE_DELAY));
 
-function eventHandle() {
-    if (!inputCountry.value==="")
-    fetchCountries(inputCountry.value.trim())
-        .then(countries => showCountries(countries))
+function eventHandle(e) {
+    if (!inputCountry.value.trim()) {
+        countryInfo.innerHTML = '';
+        countryList = '';
+    } else {
+    fetchCountries()
+        .then(name => showCountries(name))
+        .catch(error => {
+            return Notiflix.Notify.failure('Oops, there is no country with that name');
+          })}
 };
 
 function showCountries(countries) {
@@ -26,12 +31,16 @@ function showCountries(countries) {
         <spam class="country-name"> ${name}</span>
         </li>`).join("");
         countryList.innerHTML = generatedList;
-    } else {
-        countryList.innerHTML =
+        countryInfo.innerHTML = "";
+    } else if (countries.length === 1) {
+        countryInfo.innerHTML =
         countries.map(({name, capital, population, flags, languages}) =>
         `<h1>${name.common}</h1> <img src="&{flags.svg} height=45px">
         <p>Capital: ${capital}</p>
         <p> Population: ${population} </p>
-        <p> languages: ${languages}</p>`)
+        <p> languages: ${Object.values(languages)}</p>`);
+        countryList = "";
+    } else {
+        countryList.innerHTML = "";
     }
 }
