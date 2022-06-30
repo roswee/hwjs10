8e5da7a1a2bd5f8436b8bc3b2c632facb55fade1
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-30 12:12:28 +0200

    final

--- src/index.js
+++ src/index.js
@@ -9,32 +9,29 @@ const countryList = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info")
 inputCountry.addEventListener("input", debounce(eventHandle, DEBOUNCE_DELAY));
 
-function eventHandle(e) {
-    if (!inputCountry.value.trim()) {
+function eventHandle({ target: { value } }) {
+    const trimmedValue = value.trim();
+    if (!trimmedValue) {
         countryInfo.innerHTML = '';
-        countryList = '';
-    } else {
-    fetchCountries()
-        .then(name => showCountries(name))
-        .catch(error => {
-            return Notiflix.Notify.failure('Oops, there is no country with that name');
-          })}
+        countryList.innetHTML = '';
+    }
+    fetchCountries(trimmedValue).then(showCountries)
 };
 
-function showCountries(countries) {
-    if (countries.length > 10) {
+function showCountries(data) {
+    if (data.length > 10) {
         Notiflix.Notify.failure("Too many matches found. Please enter a more specific name.")
-    } else if (countries.length > 1 && countries.length < 10) {
-        const generatedList = countries.map(({name, flag}) => 
+    } else if (data.length > 1 && data.length < 10) {
+        const generatedList = data.map(({name, flags}) => 
         `<li>
         <img class="flag" src="${flags.svg}" alt="flag of ${name.common}" width=35px>
-        <spam class="country__name"> ${name}</span>
+        <spam class="country__name"> ${name.common}</span>
         </li>`).join("");
         countryList.innerHTML = generatedList;
         countryInfo.innerHTML = "";
-    } else if (countries.length === 1) {
+    } else if (data.length === 1) {
         countryInfo.innerHTML =
-        countries.map(({name, capital, population, flags, languages}) =>
+        data.map(({name, capital, population, flags, languages}) =>
         `<h1>${name.common}</h1> <img class="flag" src="${flags.svg}" width=35px>
         <p class="country__capital"> Capital: ${capital}</p>
         <p class="country__pop"> Population: ${population} </p>
